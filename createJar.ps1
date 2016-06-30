3b0ec5e79195a1e557a893aed304714031ba593f
Greg Maxey <gmaxey@electric-cloud.com>
2016-06-30 10:34:20 -0400

    Initial

---
+++ createJar.ps1
@@ -0,0 +1,68 @@
+param (
+    [string]$pluginKey = "EC-Dashboard",
+	[string]$version = "1.0",
+	[string]$description = "Plugin template for creating custom dashboards"
+)
+
+$pluginName = "${pluginKey}-${version}"
+
+function Add-Zip
+{
+    param([string]$zipfilename)
+
+    if(-not (test-path($zipfilename)))
+    {
+        set-content $zipfilename ("PK" + [char]5 + [char]6 + ("$([char]0)" * 18))
+        (dir $zipfilename).IsReadOnly = $false
+    }
+
+    $shellApplication = new-object -com shell.application
+        $zipfilenameFull = get-childitem $zipfilename
+    $zipPackage = $shellApplication.NameSpace($zipfilenameFull.FullName)
+
+        #$files = Get-ChildItem -Path $srcdir | where{! $_.PSIsContainer}
+
+    foreach($file in $input)
+    {
+            write-host $file.FullName
+                        $zipPackage.CopyHere($file.FullName)
+                        while($zipPackage.Items().Item($file.name) -eq $null){
+                                Start-sleep -seconds 1
+                        }
+            #Start-sleep -milliseconds 500
+    }
+}
+
+# Update project.xml with ec_setup.pl
+$ec_setup = Get-Content ec_setup.pl
+$a = Select-Xml -Path .\META-INF\project.xml -XPath '//value[../propertyName/text() = "ec_setup"]'
+$a.Node.'#text'=$ec_setup -join "`n"
+$a.Node.OwnerDocument.Save($a.Path)
+
+# Update plugin.xml with key, version, label, description
+$b = Select-Xml -Path .\META-INF\plugin.xml -XPath '//key'
+$b.Node.'#text' = $pluginKey
+$b.Node.OwnerDocument.Save($b.Path)
+
+$b = Select-Xml -Path .\META-INF\plugin.xml -XPath '//version'
+$b.Node.'#text' = $version
+$b.Node.OwnerDocument.Save($b.Path)
+
+$b = Select-Xml -Path .\META-INF\plugin.xml -XPath '//label'
+$b.Node.'#text' = $pluginKey
+$b.Node.OwnerDocument.Save($b.Path)
+
+$b = Select-Xml -Path .\META-INF\plugin.xml -XPath '//description'
+$b.Node.'#text' = $description
+$b.Node.OwnerDocument.Save($b.Path)
+
+del "${pluginKey}.zip" -ErrorAction SilentlyContinue
+del "${pluginKey}.jar" -ErrorAction SilentlyContinue
+dir . | Add-Zip "${pluginKey}.zip"
+Move-Item "${pluginKey}.zip" "${pluginKey}.jar"
+
+ectool uninstallPlugin "$pluginName"
+ectool installPlugin "${pluginKey}.jar"
+#ectool promotePlugin "$pluginName"
+
+type $Env:TEMP/log.txt
---
+++ ec_setup.pl
@@ -0,0 +1,37 @@
+use Cwd;
+use Data::Dumper;
+use File::Spec;
+
+my $dir = getcwd;
+my $logfile ="";
+if(defined $ENV{'QUERY_STRING'}) { # Promotion through UI
+	$logfile = "/tmp/log.txt";
+} else {
+	$logfile = "$ENV{'TEMP'}/log.txt";
+}
+open(my $fh, '>', $logfile) or die "Could not open file '$logfile' $!";
+print $fh "Plugin Name: $pluginName\n";
+print $fh "Current directory: $dir\n";
+
+if ($promoteAction eq 'promote') {
+	local $/ = undef;
+	if(defined $ENV{'QUERY_STRING'}) { # Promotion through UI
+		`cd ../../$pluginName/sweet_dashboard_project;(dashing start &> /tmp/dashing.log) &`;
+	} else {  # Promotion from the command line
+	}
+} elsif ($promoteAction eq 'demote') {
+	if(defined $ENV{'QUERY_STRING'}) { # Demotion through UI
+		`killall bundle`;
+	} else {  # Promotion from the command line
+	}
+}
+
+close $fh;
+
+# TODO: create log file output property
+open LOGFILE, $logfile or die "Couldn't open file: $!";
+my $logFileContent = <LOGFILE>;
+my $propertyResponse = $commander->setProperty("/plugins/$pluginName/projects/logfile",
+			{value=>$logFileContent}
+	);
+close LOGFILE;
