fe75245344e3de99dfe59ba88430ee163a0a7aa0
Greg Maxey <gmaxey@electric-cloud.com>
2016-07-01 08:48:49 -0400

    Working component inventory

--- dashing/jobs/component-inventory.rb
+++ dashing/jobs/component-inventory.rb
@@ -4,19 +4,48 @@ hrows = [
 
 
 SCHEDULER.every '5s', :first_in => 0 do |job|
-	rows = []
+	rowData = []
+	envs= ["DEV", "QA", "PROD"]
+	envs.each do |env|
 
-	http = Net::HTTP.new("localhost", "8000")
-	req = Net::HTTP::Get.new("/rest/v1.0/projects/SF/environments/DEV/deploymentHistoryItems?applicationName=Store%20Front&latest=false&processName=Deploy")
-	req.basic_auth "admin", "changeme"
-	response = http.request(req)
-	components = JSON.parse(response.body)["deploymentHistoryItem"][0]["deploymentDetailsItem"]
+		http = Net::HTTP.new("flow", "8000")
+		req = Net::HTTP::Get.new("/rest/v1.0/projects/SF/environments/#{env}/deploymentHistoryItems?applicationName=Store%20Front&latest=false&processName=Deploy")
+		req.basic_auth "admin", "changeme"
+		response = http.request(req)
+		components = JSON.parse(response.body)["deploymentHistoryItem"][0]["deploymentDetailsItem"]
+		rowData.push({:env => env, :comps => components})	
 
-	
-	components.each do |component|
-		rows.push({ cols: [ {value: component["componentName"]}, {value: component["actualArtifactVersion"]}, {value: rand(5)}, {value: rand(5)} ]})
 	end
 
+	# Get full component list
+	allComponents = []
+	rowData.each do |rowD|
+		rowD[:comps].each do |comp|
+			unless allComponents.include? comp["componentName"]
+				allComponents.push( comp["componentName"])
+			end 
+		end
+	end
+
+	#puts allComponents
+	#            env        comps
+	#puts rowData[0][:comps][0]["actualArtifactVersion"]
+	#puts rowData[0][:comps].find {|x| x['componentName'] == 'Vendor PL' }
+
+	rows = []
+	allComponents.each do |comp|
+		# Get values for each environment
+		#puts comp
+		vals = []
+		vals.push ({value: comp})
+		rowData.each do |env|
+			version = env[:comps].find {|x| x['componentName'] == comp }["actualArtifactVersion"]
+			vals.push ({value: version})
+		end
+		rows.push({cols: vals})
+		#puts vals
+	end
+	
 	send_event('component-inventory', { hrows: hrows, rows: rows } )
 end
 
